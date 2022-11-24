1f71d8a1cdcd6a1d77d56342eb15d9715ec5200a
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-11-24 12:03:07 -0500

    retry

---
+++ server.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import express from 'express';
+import { roll } from "./lib/roll.js";
+
+const arg = minimist(process.argv.slice(2));
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); 
+
+var port = null;
+if(arg.port){port = arg.port;}
+else{port = 5000;}
+
+app.get('/app/', (req,res)=> {res.status(200().send('200 OK'); }) 
+
+app.get('/app/roll/', (req,res) => { res.status(200).send(roll(6,2,1)) })
+
+app.get('/app/roll/', (req,res) => { 
+	if(req.body.sides){ let sides = parseInt(req.body.sides);}
+	else{ let sides = 6;}
+	if(req.body.dice) {let dice = parseInt(req.body.sides);}
+	else{let dice = 2;}
+	if(req.body.rolls){ let rolls = parseInt(req.body.rolls);}
+	else{let rolls = 1;}
+	res.status(200).send(roll(sides, dice, rolls))})
+
+app.get('/app/roll/:sides/', (req, res) => { 
+	if(req.params.sides){ let sides = parseInt(req.params.sides);}
+	else{let sides = 6;}
+	res.status(200).send(roll(sides,2,1)) })
+
+app.get('/app/roll/:sides/:dice/', (req,res) => {
+	if(req.params.sides){ let sides = parseInt(req.params.sides);}
+	else{let sides = 6;}
+	if(req.params.dice){ let dice = parseInt(req.params.dice);}
+	else{let dice = 2;}
+	res.status(200).send(roll(sides,dice,1)); } )
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+	if(req.params.sides) { let sides = parseInt(req.params.sides);}
+	else{ let sides = 6;}
+	if(req.params.dice){ let dice = parseInt(req.params.dice); }
+	else{ let dice = 2;}
+	if(req.params.rolls){let rolls = parseInt(req.params.dice);}
+	else{let rolls = 1;}
+	res.status(200).sent(roll(sides, dice, rolls)); })
+
+app.get('*', (req,res) => { res.status(404).send('404 NOT FOUND'); })
+
+app.listen(port); 
