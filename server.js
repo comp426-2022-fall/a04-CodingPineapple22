041f404f46993479c9bdcaab35421216f8e8e096
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-11-24 18:13:55 -0500

    rw

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { roll } from "./lib/roll.js";
 
 const arg = minimist(process.argv.slice(2));
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
 
 const port = arg.port || 5000; 
@@ -15,34 +16,34 @@ let sides = 6;
 let dice = 2;
 let rolls = 1; 
 
-app.get('/app/roll/', (req,res,next) => {res.setHeader('Content-Type', 'application/json');
-	res.status(200).send(roll(sides, dice, rolls)); });
+app.get('/app/roll/', (req,res) => {res.setHeader('Content-Type', 'application/json');
+	res.status(200).send(roll(sides, dice, rolls)).end(); });
 
-app.get('/app/roll/', (req,res,next) => { 
+app.get('/app/roll/', (req,res) => { 
 	sides = parseInt(req.body.sides);
 	dice = parseInt(req.body.dice);
 	rolls = parseInt(req.body.rolls);
 	res.setStatus('Content-Type', 'application/json');
-	res.status(200).send(roll(sides, dice, rolls)); });
+	res.status(200).send(roll(sides, dice, rolls)).end(); });
 
 app.get('/app/roll/:sides/', (req, res) => { 
 	sides = parseInt(req.params.sides);
 	res.setHeader('Content-Type','application/json');
-	res.status(200).send(roll(sides,2,1)); });
+	res.status(200).send(roll(sides,2,1)).end(); });
 
 app.get('/app/roll/:sides/:dice/', (req,res) => {
        sides = parseInt(req.params.sides);
        dice = parseInt(req.params.dice);
 	res.setHeader('Content-Type', 'application/json');
-	res.status(200).send(roll(sides,dice,1)); });
+	res.status(200).send(roll(sides,dice,1)).end(); });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
 	sides = parseInt(req.params.sides);
         dice = parseInt(req.params.dice);
 	rolls = parseInt(req.params.dice);
 	res.setHeader('Content-Type', 'application/json'); 
-	res.status(200).send(roll(sides, dice, rolls)); });
+	res.status(200).send(roll(sides, dice, rolls)).end(); });
 
-app.get('*', (req,res) => { res.status(404).send('404 NOT FOUND'); }); 
+app.get('*', (req,res) => { res.status(404).send('404 NOT FOUND').end(); }); 
 
 app.listen(port); 
