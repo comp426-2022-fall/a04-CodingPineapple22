c47d6f992dc0294d2392a7f09a7abf7e104add84
CodingPineapple22 <leothomasthefirst@gmail.com>
2022-11-24 22:37:56 -0500

    wf

--- server.js
+++ server.js
@@ -7,42 +7,28 @@ const arg = minimist(process.argv.slice(2));
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); 
-
 const port = arg.port || 5000; 
 
 app.get('/app/', (req,res)=> {res.status(200).send('200 OK'); }); 
 
-var sides = 6;
-var dice = 2;
-var rolls = 1; 
-
 app.get('/app/roll/', (req,res) => {res.setHeader('Content-Type', 'application/json');
-	res.status(200).send(roll(sides, dice, rolls)).end(); });
+	res.status(200).send(roll(6,2,1)); });
 
 app.get('/app/roll/', (req,res) => { 
-	sides = parseInt(req.body.sides);
-	dice = parseInt(req.body.dice);
-	rolls = parseInt(req.body.rolls);
 	res.setStatus('Content-Type', 'application/json');
-	res.status(200).send(roll(sides, dice, rolls)).end(); });
+	res.status(200).send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));  });
 
 app.get('/app/roll/:sides/', (req, res) => { 
-	sides = parseInt(req.params.sides);
 	res.setHeader('Content-Type','application/json');
-	res.status(200).send(roll(sides,2,1)).end(); });
+	res.status(200).send(roll(parseInt(req.params.sides),2,1)); });
 
 app.get('/app/roll/:sides/:dice/', (req,res) => {
-       sides = parseInt(req.params.sides);
-       dice = parseInt(req.params.dice);
 	res.setHeader('Content-Type', 'application/json');
-	res.status(200).send(roll(sides,dice,1)).end(); });
+	res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1)); });
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
-	sides = parseInt(req.params.sides);
-        dice = parseInt(req.params.dice);
-	rolls = parseInt(req.params.rolls);
 	res.setHeader('Content-Type', 'application/json'); 
-	res.status(200).send(roll(sides, dice, rolls)).end(); });
+	res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls))); });
 
 app.get('*', (req,res) => { res.status(404).send('404 NOT FOUND').end(); }); 
 
